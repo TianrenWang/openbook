7e8bf0d9e58af371afd886f07b9772b5909a6bff
Tianren Wang <kagutaba@gra-login3.graham.sharcnet>
2019-10-09 11:16:31 -0400

    Modified run.sh to fit compute canada

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+#SBATCH --gres=gpu:1        # request GPU "generic resource"
+#SBATCH --cpus-per-task=6   # maximum CPU cores per GPU request: 6 on Cedar, 16$
+#SBATCH --mem=32000M        # memory per node
+#SBATCH --time=0-01:00      # time (DD-HH:MM)
+#SBATCH --output=output.out  # %N for node name, %j for jobID
+
 #### local path
 #RACE_DIR=../RACE
 #INIT_CKPT_DIR=../xlnet_cased_L-24_H-1024_A-16
