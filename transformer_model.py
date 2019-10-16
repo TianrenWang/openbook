da8380e879967c9bd1955230b9234513ca17a949
TianrenWang <broodstar_frank@hotmail.com>
2019-10-15 22:28:28 -0400

    Fixed a typo that I used wrong layer for normalization

--- transformer_model.py
+++ transformer_model.py
@@ -238,8 +238,8 @@ def TED_generator(vocab_size, FLAGS):
             self.mha = MultiHeadAttention(d_model, num_heads)
             self.ffn = point_wise_feed_forward_network(d_model, dff)
 
-            self.layernorm1 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
-            self.layernorm2 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
+            self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+            self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
             self.dropout1 = tf.keras.layers.Dropout(rate)
             self.dropout2 = tf.keras.layers.Dropout(rate)
@@ -247,11 +247,11 @@ def TED_generator(vocab_size, FLAGS):
         def call(self, x, training, mask):
             attn_output, _ = self.mha(x, x, x, mask)  # (batch_size, input_seq_len, d_model)
             attn_output = self.dropout1(attn_output, training=training)
-            out1 = self.layernorm1(x + attn_output, training=training)  # (batch_size, input_seq_len, d_model)
+            out1 = self.layernorm1(x + attn_output)  # (batch_size, input_seq_len, d_model)
 
             ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
             ffn_output = self.dropout2(ffn_output, training=training)
-            out2 = self.layernorm2(out1 + ffn_output, training=training)  # (batch_size, input_seq_len, d_model)
+            out2 = self.layernorm2(out1 + ffn_output)  # (batch_size, input_seq_len, d_model)
 
             return out2
 
@@ -282,9 +282,9 @@ def TED_generator(vocab_size, FLAGS):
 
             self.ffn = point_wise_feed_forward_network(d_model, dff)
 
-            self.layernorm1 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
-            self.layernorm2 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
-            self.layernorm3 = tf.keras.layers.BatchNormalization(epsilon=1e-6)
+            self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+            self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+            self.layernorm3 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
 
             self.dropout1 = tf.keras.layers.Dropout(rate)
             self.dropout2 = tf.keras.layers.Dropout(rate)
@@ -296,16 +296,16 @@ def TED_generator(vocab_size, FLAGS):
 
             attn1, attn_weights_block1 = self.mha1(x, x, x, look_ahead_mask)  # (batch_size, target_seq_len, d_model)
             attn1 = self.dropout1(attn1, training=training)
-            out1 = self.layernorm1(attn1 + x, training=training)
+            out1 = self.layernorm1(attn1 + x)
 
             attn2, attn_weights_block2 = self.mha2(
                 enc_output, enc_output, out1, padding_mask)  # (batch_size, target_seq_len, d_model)
             attn2 = self.dropout2(attn2, training=training)
-            out2 = self.layernorm2(attn2 + out1, training=training)  # (batch_size, target_seq_len, d_model)
+            out2 = self.layernorm2(attn2 + out1)  # (batch_size, target_seq_len, d_model)
 
             ffn_output = self.ffn(out2)  # (batch_size, target_seq_len, d_model)
             ffn_output = self.dropout3(ffn_output, training=training)
-            out3 = self.layernorm3(ffn_output + out2, training=training)  # (batch_size, target_seq_len, d_model)
+            out3 = self.layernorm3(ffn_output + out2)  # (batch_size, target_seq_len, d_model)
 
             return out3, attn_weights_block1, attn_weights_block2
 
