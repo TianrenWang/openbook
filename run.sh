29acbde88ad5bb5f52eecb3a62360654a27aead2
TianrenWang <broodstar_frank@hotmail.com>
2020-02-03 23:01:39 -0500

    Revert "Modified learning rate for embedding"
    This reverts commit 574f05562089bdccff3ec481aebc9c8371a0b4e3.
    
    Modifying learning rate actually does nothing.

--- run.sh
+++ run.sh
@@ -36,7 +36,6 @@ python3 estimator.py \
   --sparse_loss=1e-5 \
   --update_loss=1e-5 \
   --alpha=0.9 \
-  --embed_rate=1e-3 \
   --graph_size=512 \
   --batch_size=128 \
   --embed_batch_size=32 \
